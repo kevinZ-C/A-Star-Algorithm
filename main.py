42dcde025efa591e09d7b2c8d765714108862af8
Kevin-1219 <1420261524@qq.com>
2023-06-28 21:35:59 +0800

    自定义起点和终点进行机房环境下的A*算法模拟 初步假设机器人只有四自由度（上下左右），所以对应的A*算法也只有四搜索方向

--- main.py
+++ main.py
@@ -25,17 +25,33 @@ for i in range(map.size):
             rec = Rectangle((i, j), width=1, height=1, edgecolor='gray', facecolor='w')
             ax.add_patch(rec)
 
-rec = Rectangle((0, 0), width = 1, height = 1, facecolor='b')
+# 起点上色
+# rec = Rectangle((0, 0), width = 1, height = 1, facecolor='b')
+# ax.add_patch(rec)
+
+# 终点标红
+# rec = Rectangle((map.size-1, map.size-1), width = 1, height = 1, facecolor='r')
+# ax.add_patch(rec)
+
+# plt.axis('equal')
+# plt.axis('off')
+# plt.tight_layout()
+#plt.show()
+
+start_x = int(input("输入起点的横坐标："))
+start_y = int(input("输入起点的纵坐标："))
+rec = Rectangle((start_x, start_y), width = 1, height = 1, facecolor='b')
 ax.add_patch(rec)
 
-rec = Rectangle((map.size-1, map.size-1), width = 1, height = 1, facecolor='r')
+end_x = int(input("输入终点的横坐标："))
+end_y = int(input("输入终点的纵坐标："))
+rec = Rectangle((end_x, end_y), width = 1, height = 1, facecolor='r')
 ax.add_patch(rec)
 
 plt.axis('equal')
 plt.axis('off')
 plt.tight_layout()
-#plt.show()
 
-a_star = a_star.AStar(map)
-a_star.RunAndSaveImage(ax, plt)
+a_star = a_star.AStar(map, start_x, start_y, end_x, end_y)
+a_star.RunAndSaveImage(ax, plt, start_x, start_y)
 
